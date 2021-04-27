a36ff4fbf946e2c27a4ff3d156c95282edc869b2
nvdinh185 <nvdinh0766777123@gmail.com>
2021-04-27 17:11:14 +0700

    review and edit word

--- src/app/words/words.component.ts
+++ src/app/words/words.component.ts
@@ -11,12 +11,12 @@ export class WordsComponent {
   isShow = false;
   filterStatus = "tat_ca";
   arWords = [
-    { id: 1, en: 'action', vn: 'hành động', memorized: true },
-    { id: 2, en: 'actor', vn: 'diễn viên', memorized: false },
-    { id: 3, en: 'activity', vn: 'hoạt động', memorized: true },
-    { id: 4, en: 'active', vn: 'chủ động', memorized: true },
-    { id: 5, en: 'bath', vn: 'tắm', memorized: false },
-    { id: 6, en: 'bedroom', vn: 'phòng ngủ', memorized: true }
+    { id: 1, en: 'action', vn: 'hành động', memorized: true, onEdit: false },
+    { id: 2, en: 'actor', vn: 'diễn viên', memorized: false, onEdit: false },
+    { id: 3, en: 'activity', vn: 'hoạt động', memorized: true, onEdit: false },
+    { id: 4, en: 'active', vn: 'chủ động', memorized: true, onEdit: false },
+    { id: 5, en: 'bath', vn: 'tắm', memorized: false, onEdit: false },
+    { id: 6, en: 'bedroom', vn: 'phòng ngủ', memorized: true, onEdit: false }
   ];
 
   constructor() { }
@@ -25,7 +25,7 @@ export class WordsComponent {
    * Thêm từ mới vào danh sách
    */
   addWord() {
-    let word = { id: this.arWords.length + 1, en: this.en, vn: this.vn, memorized: false };
+    let word = { id: this.arWords.length + 1, en: this.en, vn: this.vn, memorized: false, onEdit: false };
     this.arWords.unshift(word);
     this.isShow = !this.isShow;
     this.en = '';
@@ -37,8 +37,35 @@ export class WordsComponent {
    * @param id 
    */
   deleteWord(id) {
-    let index = this.arWords.findIndex(e => e.id==id);
+    let index = this.arWords.findIndex(e => e.id == id);
     this.arWords.splice(index, 1);
+  }
+
+  /**
+   * Hiển thị form sửa
+   * @param id 
+   */
+  edit(id) {
+    let index = this.arWords.findIndex(e => e.id == id);
+    this.en = this.arWords[index].en;
+    this.vn = this.arWords[index].vn;
+    this.arWords[index].onEdit = true;
+  }
+
+  /**
+   * Sửa từ
+   * @param id 
+   */
+  editWord(id) {
+    let index = this.arWords.findIndex(e => e.id == id);
+    let newWord = {
+      id: this.arWords[index].id, en: this.en, vn: this.vn,
+      memorized: this.arWords[index].memorized, onEdit: this.arWords[index].onEdit
+    };
+    this.arWords = this.arWords.map(word => (word.id === newWord.id ? newWord : word));
+    this.arWords[index].onEdit = false;
+    this.en = '';
+    this.vn = '';
   }
 
   /**
